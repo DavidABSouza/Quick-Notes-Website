9e9e3176a79ac2359d746150fc3fcf7a6fed04d0
DavidABSouza <superdave300py@gmail.com>
2026-04-18 21:39:40 +0100

    feat: Finalização do código de JavaScript

--- scriptQN.js
+++ scriptQN.js
@@ -1,7 +1,7 @@
-function AdicionarNota(){
+function AdicionarNota() {
     const Card = document.createElement('div')
     Card.classList.add("Card")
-    
+
     const DivCards = document.getElementById("DivNotas")
 
     let inputTitulo = document.querySelector("#inputTitulo").value
@@ -9,11 +9,32 @@ function AdicionarNota(){
     let infoData = new Date().toLocaleString()
 
     Card.innerHTML = `
-                <h3>${inputTitulo}</h3> 
+                <div onmouseover="AparecerApagar(this)" onmouseleave="SumirApagar(this)">
+                <h3>${inputTitulo}</h3>
+                <button class="botaoDeletar" type="submit" onclick="Deletar(this)">Apagar Nota</button>
                 <p>${inputTexto}</p>
                 <p>${infoData}</p>
+                </div>
     `
     DivCards.appendChild(Card)
 
 
+}
+
+function Deletar(botao) {
+
+    const Card = botao.parentElement.parentElement
+    Card.remove()
+}
+
+function AparecerApagar(div){
+
+    const botaoApagar = div.querySelector(".botaoDeletar")
+    botaoApagar.style.visibility = "visible"
+}
+
+function SumirApagar(div){
+
+    const botaoApagar = div.querySelector(".botaoDeletar")
+    botaoApagar.style.visibility = "hidden"
 }
